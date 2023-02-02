50d7ff45e6a415c0363724d4d29cd5744528ac3d
caiozim112 <caioopua@gmail.com>
2023-02-01 23:43:14 -0300

    sss

--- Game/setup/db/transactions.sql
+++ Game/setup/db/transactions.sql
@@ -1,24 +1,24 @@
 
--- Transaction Criar novo Jogador
+-- -- Transaction Criar novo Jogador
 
-CREATE OR REPLACE PROCEDURE criar_jogador(_nome_jogador nome)
-AS $criar_novo_jogador$
-  DECLARE
-  my_variable INTEGER;
-BEGIN
-  INSERT INTO personagem (nome,id_instancia_regiao,nome_regiao,tipo) VALUES(_nome_jogador,'2','Prédio governamental da folha','atacante');
-  INSERT INTO inventario DEFAULT VALUES;
-  SELECT MAX(id) INTO my_variable FROM inventario;
-INSERT INTO atacante (nome_atacante,nivel,vida,chakra,defesa,ataque,id_inventario,tipo) VALUES(_nome_jogador, 30, 100, 90, 50, 80, my_variable, 'personagem_principal');
-INSERT INTO personagem_principal(nome_personagem,dinheiro,experiencia,descricao) VALUES(_nome_jogador,0,0,'personagem jogavel');
-END;
-$$ LANGUAGE plpgsql;
+-- CREATE OR REPLACE PROCEDURE criar_jogador(_nome_jogador nome)
+-- AS $criar_novo_jogador$
+--   DECLARE
+--   my_variable INTEGER;
+-- BEGIN
+--   INSERT INTO personagem (nome,id_instancia_regiao,nome_regiao,tipo) VALUES(_nome_jogador,'2','Prédio governamental da folha','atacante');
+--   INSERT INTO inventario DEFAULT VALUES;
+--   SELECT MAX(id) INTO my_variable FROM inventario;
+-- INSERT INTO atacante (nome_atacante,nivel,vida,chakra,defesa,ataque,id_inventario,tipo) VALUES(_nome_jogador, 30, 100, 90, 50, 80, my_variable, 'personagem_principal');
+-- INSERT INTO personagem_principal(nome_personagem,dinheiro,experiencia,descricao) VALUES(_nome_jogador,0,0,'personagem jogavel');
+-- END;
+-- $$ LANGUAGE plpgsql;
 
 
-CREATE OR REPLACE FUNCTION criar_novo_jogador(_nome_jogador nome)
-AS $$
-    RETURN (
-    BEGIN;
-    CALL criar_novo_jogador(_nome_jogador);
-    COMMIT;
-    );
+-- CREATE OR REPLACE FUNCTION criar_novo_jogador(_nome_jogador nome)
+-- AS $$
+--     RETURN (
+--     BEGIN;
+--     CALL criar_novo_jogador(_nome_jogador);
+--     COMMIT;
+--     );
